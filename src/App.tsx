01b450f75766b2f5f7ea7d7a627118ba8ea11b6a
Debanshu Datta <debanshudatta123@gmail.com>
2021-10-20 23:15:51 +0530

    Final template changes

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,10 @@ import './styles.css'
 export const App = () => {
   const num = 'hello'
   return (
-    <h1>
-      Hello world!-{num} {process.env.NODE_ENV} {process.env.name}
-    </h1>
+    <>
+      <h1>
+        Hello world!-{num} {process.env.NODE_ENV} {process.env.name}
+      </h1>
+    </>
   )
 }
